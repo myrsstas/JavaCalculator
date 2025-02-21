5557fc6097e625378f3afd1f34612d5bf7d4728f
Myrsini Stasinou <myrsinistas@gmail.com>
2025-02-22 00:51:24 +0200

    Initial Commit

---
+++ src/Calculator.java
@@ -0,0 +1,73 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Calculator extends JFrame {
+
+    private JTextField tfFirstNumber;
+    private JTextField tfSecondNumber;
+    private JButton addition;
+    private JButton subtraction;
+    private JButton multiplication;
+    private JButton division;
+    private JPanel calculatorPanel;
+    private JTextField tfResult;
+
+    public Calculator() {
+        setContentPane(calculatorPanel);
+        setSize(350, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        addition.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double firstNumber = Double.parseDouble(tfFirstNumber.getText());
+                double secondNumber = Double.parseDouble(tfSecondNumber.getText());
+                tfResult.setText(String.valueOf(firstNumber + secondNumber));
+
+            }
+        });
+        subtraction.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double firstNumber = Double.parseDouble(tfFirstNumber.getText());
+                double secondNumber = Double.parseDouble(tfSecondNumber.getText());
+                tfResult.setText(String.valueOf(firstNumber - secondNumber));
+
+
+            }
+        });
+        multiplication.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double firstNumber = Double.parseDouble(tfFirstNumber.getText());
+                double secondNumber = Double.parseDouble(tfSecondNumber.getText());
+                tfResult.setText(String.valueOf(firstNumber * secondNumber));
+
+
+            }
+        });
+        division.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double firstNumber = Double.parseDouble(tfFirstNumber.getText());
+                double secondNumber = Double.parseDouble(tfSecondNumber.getText());
+
+                if (secondNumber!= 0.0) {
+                    tfResult.setText(String.valueOf(firstNumber / secondNumber));
+
+                } else {
+                    tfResult.setText("Undefined");
+
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+    }
+
+}
